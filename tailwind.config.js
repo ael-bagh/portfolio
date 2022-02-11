6cf0869189747ed4da640b762f5fc118fcdfb34f
ael-bagh <ael-bagh@student.1337.ma>
2022-02-11 03:47:29 +0100

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        thatpurple : '#7f5af0',
+        thatwhite : '#fffffe',
+        thatgreen : '#2cb67d',
+        darkbg : '#16161a',
+        lightbg : '#242629',
+        paragraph : '#94a1b2'
+      }
+    },
+  },
+  plugins: [],
+}
